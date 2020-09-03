95598239f80fb0504908ad3840f0cdd0c58ef338
kyle-escobar <kslrtips@gmail.com>
2020-09-03 00:50:48 -0400

    Finish base state recorder.

--- asm-simulator/src/main/kotlin/org/spectral/asm/simulator/ExecState.kt
+++ asm-simulator/src/main/kotlin/org/spectral/asm/simulator/ExecState.kt
@@ -8,14 +8,14 @@ import org.spectral.asm.core.Class
  * @property locals Array<Class>
  * @property localVarIds IntArray
  * @property stack Array<Class>
- * @property localStackIds IntArray
+ * @property stackVarIds IntArray
  * @constructor
  */
 data class ExecState(
         val locals: Array<Class?>,
         val localVarIds: IntArray,
         val stack: Array<Class?>,
-        val localStackIds: IntArray
+        val stackVarIds: IntArray
 ) {
     /**
      * Secondary constructor.
@@ -29,10 +29,10 @@ data class ExecState(
      * @constructor
      */
     constructor(
-            locals: Array<Class>,
+            locals: Array<Class?>,
             localVarIds: IntArray,
             localsSize: Int,
-            stack: Array<Class>,
+            stack: Array<Class?>,
             stackVarIds: IntArray,
             stackSize: Int
     ) : this(
@@ -51,7 +51,7 @@ data class ExecState(
         if (!locals.contentEquals(other.locals)) return false
         if (!localVarIds.contentEquals(other.localVarIds)) return false
         if (!stack.contentEquals(other.stack)) return false
-        if (!localStackIds.contentEquals(other.localStackIds)) return false
+        if (!stackVarIds.contentEquals(other.stackVarIds)) return false
 
         return true
     }
@@ -60,7 +60,7 @@ data class ExecState(
         var result = locals.contentHashCode()
         result = 31 * result + localVarIds.contentHashCode()
         result = 31 * result + stack.contentHashCode()
-        result = 31 * result + localStackIds.contentHashCode()
+        result = 31 * result + stackVarIds.contentHashCode()
         return result
     }
 
