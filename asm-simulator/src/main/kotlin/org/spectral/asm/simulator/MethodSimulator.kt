7ca491a14be4610398f22d843f5b7208ef780741
kyle-escobar <kslrtips@gmail.com>
2020-09-04 00:32:00 -0400

    Add abstract value base

--- asm-simulator/src/main/kotlin/org/spectral/asm/simulator/MethodSimulator.kt
+++ asm-simulator/src/main/kotlin/org/spectral/asm/simulator/MethodSimulator.kt
@@ -1,43 +1,7 @@
 package org.spectral.asm.simulator
 
-import org.objectweb.asm.tree.analysis.Analyzer
+import me.coley.analysis.SimInterpreter
 import org.spectral.asm.core.Method
-import org.spectral.asm.simulator.controlflow.BlockHandler
-import org.spectral.asm.simulator.value.AbstractValue
 
-class MethodSimulator
-
-/**
- * Private constructor.
- *
- * @property method Method
- * @property interpreter ExecutionInterpreter
- * @constructor
- */
-private constructor(
-        val method: Method,
-        val interpreter: ExecutionInterpreter
-) : Analyzer<AbstractValue>(interpreter) {
-
-    /**
-     * The control flow block handler.
-     */
-    val blockHandler = BlockHandler(method)
-
-    /**
-     * Primary constructor.
-     *
-     * @param method Method
-     * @constructor
-     */
-    constructor(method: Method) : this(method, ExecutionInterpreter()) {
-        this.interpreter.simulator = this
-        this.interpreter.blockHandler = blockHandler
-    }
-
-    companion object {
-        /**
-         * Common class types.
-         */
-    }
+class MethodSimulator private constructor(val method: Method) {
 }
--- asm-simulator/src/main/kotlin/org/spectral/asm/simulator/value/AbstractValue.kt
+++ asm-simulator/src/main/kotlin/org/spectral/asm/simulator/value/AbstractValue.kt
@@ -4,68 +4,79 @@ import org.objectweb.asm.Type
 import org.objectweb.asm.tree.AbstractInsnNode
 import org.objectweb.asm.tree.JumpInsnNode
 import org.objectweb.asm.tree.analysis.Value
-import org.spectral.asm.core.Class
 import org.spectral.asm.core.ext.slotSize
 
 /**
- * Represents an abstract value at a given frame.
+ * Represents an abstract implementation of a value of a local variable or stack element
+ * in the LVT or stack at a given frame execution.
  *
- * @property insns MutableList<AbstractInsnNode>
- * @property element Class
+ * @property insns List<AbstractInsnNode>
+ * @property type Type
  * @property value Any?
  * @constructor
  */
-abstract class AbstractValue(val insns: MutableList<AbstractInsnNode>, val element: Class?, val value: Any?) : Value {
+abstract class AbstractValue(val insns: List<AbstractInsnNode>, val type: Type, val value: Any?) : Value {
 
     /**
-     * Creates a value with an optional instruction
+     * Creates a value instance with a single instruction.
      *
-     * @param insn AbstractInsnNode?
-     * @param element Class
+     * @param insn AbstractInsnNode
+     * @param type Type
      * @param value Any?
      * @constructor
      */
-    constructor(insn: AbstractInsnNode?, element: Class?, value: Any?) : this(
-            if(insn == null) mutableListOf() else mutableListOf(insn),
-            element,
-            value
-    )
+    constructor(insn: AbstractInsnNode, type: Type, value: Any?) : this(listOf(insn), type, value)
+
+    /**
+     * Creates a value instance witout any defined instrutions.
+     *
+     * @param type Type
+     * @param value Any?
+     * @constructor
+     */
+    constructor(type: Type, value: Any?) : this(listOf(), type, value)
+
+    /**
+     * Creates an empty null value instance.
+     *
+     * @constructor
+     */
+    constructor() : this(listOf(), Type.getObjectType("null"), null)
 
     var nullCheck: JumpInsnNode? = null
-        set(value) {
-            field = value
-            if(copySource != null) {
-                copySource!!.nullCheck = value
-            }
-        }
+    private set
+
+    var copySource: AbstractValue? = null
+    private set
 
     /**
-     * The source of the copy action for this value.
+     * The instructions which pushed this value to the stack.
      */
-    private var copySource: AbstractValue? = null
+    val pushed = insns
 
     /**
-     * Copies the instruction from the top of the stack.
-     *
-     * @param insn AbstractInsnNode
-     * @return AbstractValue
+     * The instructions which have popped this value off the stack.
      */
-    abstract fun copy(insn: AbstractInsnNode): AbstractValue
+    val pops = mutableListOf<AbstractInsnNode>()
 
     /**
-     * Invoked when the value is copied
-     *
-     * @param copy AbstractValue
-     * @return AbstractValue
+     * Whether the value type is primitive or not
      */
-    fun onCopy(copy: AbstractValue): AbstractValue {
-        copy.nullCheck = nullCheck
-        copy.copySource = this
-        return copy
-    }
+    abstract val isPrimitive: Boolean
 
     /**
-     * Whether the given [other] can be merged with the current value.
+     * Whether the value type is a reference type or not
+     */
+    abstract val isReference: Boolean
+
+    /**
+     * Whether the value is resolved or not.
+     */
+    abstract val isValueResolved: Boolean
+
+    /**
+     * Gets whether this value type is mergeable with the
+     * provided [other] value object.
      *
      * @param other AbstractValue
      * @return Boolean
@@ -73,19 +84,39 @@ abstract class AbstractValue(val insns: MutableList<AbstractInsnNode>, val eleme
     abstract fun canMerge(other: AbstractValue): Boolean
 
     /**
-     * Whether the current value is a primitive data type.
+     * Copies this value given an instruction type.
+     *
+     * @param insn AbstractInsnNode
+     * @return AbstractValue
      */
-    abstract val isPrimitive: Boolean
+    abstract fun copy(insn: AbstractInsnNode): AbstractValue
 
     /**
-     * Whether the current value is a reference to another type.
+     * Invoked when this value is duped on the stack.
+     *
+     * @param copy AbstractValue
+     * @return AbstractValue
      */
-    abstract val isReference: Boolean
+    internal fun onCopy(copy: AbstractValue): AbstractValue {
+        copy.setNullCheckedBy(nullCheck)
+        copy.copySource = this
+        return copy
+    }
 
     /**
-     * Whether the current value has a resolved data value.
+     * Sets the null check control flow jump
+     *
+     * @param nullCheck JumpInsnNode
      */
-    abstract val isValueResolved: Boolean
+    fun setNullCheckedBy(nullCheck: JumpInsnNode?) {
+        this.nullCheck = nullCheck
+        copySource?.setNullCheckedBy(nullCheck)
+    }
+
+    /**
+     * Gets whether the value is unresolved or not.
+     */
+    val isValueUnresolved: Boolean get() = !isValueResolved
 
     /**
      * Whether the data value is null or not.
@@ -93,17 +124,7 @@ abstract class AbstractValue(val insns: MutableList<AbstractInsnNode>, val eleme
     val isNull: Boolean get() = value == null
 
     /**
-     * Whether the value is an array type.
-     */
-    val isArray: Boolean get() = element?.isArray ?: false
-
-    /**
-     * The ASM [Type] of the value's class
-     */
-    val type: Type get() = element?.type ?: Type.getObjectType("null")
-
-    /**
-     * Gets the element size on the stack.
+     * Gets the size of the value on the stack.
      *
      * @return Int
      */
