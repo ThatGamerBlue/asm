34e7d5dc7b781a85e17e4c9c6b139686901fea12
kyle-escobar <kslrtips@gmail.com>
2020-09-04 16:04:03 -0400

    Add new operation support to the interpreter

--- asm-simulator/src/main/kotlin/org/spectral/asm/simulator/ExecInterpreter.kt
+++ asm-simulator/src/main/kotlin/org/spectral/asm/simulator/ExecInterpreter.kt
@@ -1,16 +1,15 @@
 package org.spectral.asm.simulator
 
-import org.objectweb.asm.Opcodes.ASM8
+import org.objectweb.asm.ConstantDynamic
+import org.objectweb.asm.Handle
+import org.objectweb.asm.Opcodes.*
 import org.objectweb.asm.Type
-import org.objectweb.asm.tree.AbstractInsnNode
-import org.objectweb.asm.tree.TryCatchBlockNode
+import org.objectweb.asm.tree.*
+import org.objectweb.asm.tree.analysis.AnalyzerException
 import org.objectweb.asm.tree.analysis.Frame
 import org.objectweb.asm.tree.analysis.Interpreter
 import org.spectral.asm.simulator.controlflow.BlockHandler
-import org.spectral.asm.simulator.value.AbstractValue
-import org.spectral.asm.simulator.value.PrimitiveValue
-import org.spectral.asm.simulator.value.UninitializedValue
-import org.spectral.asm.simulator.value.VirtualValue
+import org.spectral.asm.simulator.value.*
 
 /**
  * Responsible for interpreting bytecode of a method and calculating the
@@ -89,8 +88,53 @@ class ExecInterpreter : Interpreter<AbstractValue>(ASM8) {
         return newValue(tryCatchBlockNode.handler, exceptionType)
     }
 
-    override fun newOperation(insn: AbstractInsnNode?): AbstractValue {
-        TODO("Not yet implemented")
+    override fun newOperation(insn: AbstractInsnNode): AbstractValue?  {
+        return when(insn.opcode) {
+            ACONST_NULL -> NullConstantValue.newNull(insn)
+            ICONST_M1 -> PrimitiveValue.ofInt(insn, -1)
+            ICONST_0 -> PrimitiveValue.ofInt(insn, 0)
+            ICONST_1 -> PrimitiveValue.ofInt(insn, 1)
+            ICONST_2 -> PrimitiveValue.ofInt(insn, 2)
+            ICONST_3 -> PrimitiveValue.ofInt(insn, 3)
+            ICONST_4 -> PrimitiveValue.ofInt(insn, 4)
+            ICONST_5 -> PrimitiveValue.ofInt(insn, 5)
+            LCONST_0 -> PrimitiveValue.ofLong(insn, 0L)
+            LCONST_1 -> PrimitiveValue.ofLong(insn, 1L)
+            FCONST_0 -> PrimitiveValue.ofFloat(insn, 0.0F)
+            FCONST_1 -> PrimitiveValue.ofFloat(insn, 1.0F)
+            FCONST_2 -> PrimitiveValue.ofFloat(insn, 2.0F)
+            DCONST_0 -> PrimitiveValue.ofDouble(insn, 0.0)
+            DCONST_1 -> PrimitiveValue.ofDouble(insn, 1.0)
+            BIPUSH, SIPUSH -> PrimitiveValue.ofInt(insn, (insn as IntInsnNode).operand)
+            LDC -> {
+                when(val value = (insn as LdcInsnNode).cst) {
+                    is Int -> PrimitiveValue.ofInt(insn, value.toInt())
+                    is Long -> PrimitiveValue.ofLong(insn, value.toLong())
+                    is Float -> PrimitiveValue.ofFloat(insn, value.toFloat())
+                    is Double -> PrimitiveValue.ofDouble(insn, value.toDouble())
+                    is String -> throw UnsupportedOperationException("String support not implemented into interpreter yet.")
+                    is Type -> {
+                        val type = value as Type
+                        if(type.sort == Type.OBJECT || type.sort == Type.ARRAY) {
+                            return VirtualValue.ofClass(insn, type)
+                        }
+                        else if(type.sort == Type.METHOD) {
+                            newValue(insn, Type.getObjectType("java/lang/invoke/MethodType"))!!
+                        }
+                        else {
+                            throw AnalyzerException(insn, "Illegal LDC value $value")
+                        }
+                    }
+                    is Handle -> newValue(insn, Type.getObjectType("java/lang/invoke/MethodHandle"))
+                    is ConstantDynamic -> newValue(insn, Type.getType(value.descriptor))
+                    else -> throw AnalyzerException(insn, "Illegal LDC value $value")
+                }
+            }
+            JSR -> ReturnAddressValue.newRet(insn)
+            GETSTATIC -> newValue(insn, Type.getType((insn as FieldInsnNode).desc))
+            NEW -> newValue(insn, Type.getObjectType((insn as TypeInsnNode).desc))
+            else -> throw IllegalStateException()
+        }
     }
 
     override fun copyOperation(insn: AbstractInsnNode?, value: AbstractValue?): AbstractValue {
