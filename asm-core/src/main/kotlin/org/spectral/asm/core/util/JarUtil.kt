4d27ebcada94b712b0c50ecd3e0f77d7161c98a9
kyle-escobar <kslrtips@gmail.com>
2020-09-17 15:48:03 -0400

    Add jar util

---
+++ asm-core/src/main/kotlin/org/spectral/asm/core/util/JarUtil.kt
@@ -0,0 +1,35 @@
+package org.spectral.asm.core.util
+
+import org.objectweb.asm.ClassReader
+import org.spectral.asm.core.Class
+import org.spectral.asm.core.ClassPool
+import java.io.File
+import java.io.FileNotFoundException
+import java.util.jar.JarFile
+
+object JarUtil {
+
+    fun readJar(filePath: String): ClassPool {
+        val pool = ClassPool()
+        val file = File(filePath)
+
+        if(!file.exists()) {
+            throw FileNotFoundException("Jar file at path '$filePath' not found.")
+        }
+
+        JarFile(file).use { jar ->
+            jar.entries().asSequence()
+                    .filter { it.name.endsWith(".class") }
+                    .forEach {
+                        val cls = Class(pool)
+                        val reader = ClassReader(jar.getInputStream(it))
+
+                        reader.accept(cls, ClassReader.SKIP_FRAMES)
+
+                        pool.add(cls)
+                    }
+        }
+
+        return pool
+    }
+}
