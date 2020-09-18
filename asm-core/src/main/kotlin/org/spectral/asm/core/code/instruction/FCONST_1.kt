85bc0c087c54a5a19601cf41e58704455dae21a4
kyle-escobar <kslrtips@gmail.com>
2020-09-17 21:11:05 -0400

    Add float constant instruction type

--- asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/FCONST_1.kt
+++ asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/FCONST_1.kt
@@ -3,12 +3,16 @@ package org.spectral.asm.core.code.instruction
 import kotlin.String
 import org.objectweb.asm.MethodVisitor
 import org.spectral.asm.core.code.Instruction
+import org.spectral.asm.core.code.type.ConstantInstruction
 import org.spectral.asm.core.common.Opcode
 
 @Opcode(value=12)
-class FCONST_1 : Instruction(12) {
-  override fun accept(visitor: MethodVisitor) {
+class FCONST_1 : Instruction(12), ConstantInstruction {
 
+  override val cst: Float = 1F
+
+  override fun accept(visitor: MethodVisitor) {
+    visitor.visitInsn(opcode)
   }
 
   override fun toString(): String = "FCONST_1"
--- asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/FCONST_2.kt
+++ asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/FCONST_2.kt
@@ -3,12 +3,16 @@ package org.spectral.asm.core.code.instruction
 import kotlin.String
 import org.objectweb.asm.MethodVisitor
 import org.spectral.asm.core.code.Instruction
+import org.spectral.asm.core.code.type.ConstantInstruction
 import org.spectral.asm.core.common.Opcode
 
 @Opcode(value=13)
-class FCONST_2 : Instruction(13) {
-  override fun accept(visitor: MethodVisitor) {
+class FCONST_2 : Instruction(13), ConstantInstruction {
 
+  override val cst: Float = 2F
+
+  override fun accept(visitor: MethodVisitor) {
+    visitor.visitInsn(opcode)
   }
 
   override fun toString(): String = "FCONST_2"
