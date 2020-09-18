58ed35e7c5a1dcfee54f2989725c8410526f09fe
kyle-escobar <kslrtips@gmail.com>
2020-09-17 21:51:58 -0400

    Add int instruction type

--- asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/SIPUSH.kt
+++ asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/SIPUSH.kt
@@ -3,12 +3,14 @@ package org.spectral.asm.core.code.instruction
 import kotlin.String
 import org.objectweb.asm.MethodVisitor
 import org.spectral.asm.core.code.Instruction
+import org.spectral.asm.core.code.type.IntInstruction
 import org.spectral.asm.core.common.Opcode
 
 @Opcode(value=17)
-class SIPUSH : Instruction(17) {
-  override fun accept(visitor: MethodVisitor) {
+class SIPUSH(override val operand: Int) : Instruction(17), IntInstruction {
 
+  override fun accept(visitor: MethodVisitor) {
+    visitor.visitIntInsn(opcode, operand)
   }
 
   override fun toString(): String = "SIPUSH"
