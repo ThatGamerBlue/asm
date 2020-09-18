2ed516ccb3cb2c3ce086b22211b68366ec492a7b
kyle-escobar <kslrtips@gmail.com>
2020-09-17 20:58:39 -0400

    Add long constant instruction type

--- asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/LCONST_0.kt
+++ asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/LCONST_0.kt
@@ -3,12 +3,16 @@ package org.spectral.asm.core.code.instruction
 import kotlin.String
 import org.objectweb.asm.MethodVisitor
 import org.spectral.asm.core.code.Instruction
+import org.spectral.asm.core.code.type.ConstantInstruction
 import org.spectral.asm.core.common.Opcode
 
 @Opcode(value=9)
-class LCONST_0 : Instruction(9) {
-  override fun accept(visitor: MethodVisitor) {
+class LCONST_0 : Instruction(9), ConstantInstruction {
 
+  override val cst: Long = 0L
+
+  override fun accept(visitor: MethodVisitor) {
+    visitor.visitInsn(opcode)
   }
 
   override fun toString(): String = "LCONST_0"
