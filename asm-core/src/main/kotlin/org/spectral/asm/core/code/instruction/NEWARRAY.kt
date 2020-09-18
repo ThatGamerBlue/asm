01490dd57624c60a1be57c125fbbe657c7087d44
kyle-escobar <kslrtips@gmail.com>
2020-09-17 22:21:02 -0400

    Add int instruction

--- asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/NEWARRAY.kt
+++ asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/NEWARRAY.kt
@@ -3,12 +3,14 @@ package org.spectral.asm.core.code.instruction
 import kotlin.String
 import org.objectweb.asm.MethodVisitor
 import org.spectral.asm.core.code.Instruction
+import org.spectral.asm.core.code.type.IntInstruction
 import org.spectral.asm.core.common.Opcode
 
 @Opcode(value=188)
-class NEWARRAY : Instruction(188) {
-  override fun accept(visitor: MethodVisitor) {
+class NEWARRAY(override val operand: Int) : Instruction(188), IntInstruction {
 
+  override fun accept(visitor: MethodVisitor) {
+    visitor.visitIntInsn(opcode, operand)
   }
 
   override fun toString(): String = "NEWARRAY"
