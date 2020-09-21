d81f6265620bbb4cd769849220616ffbc5e2771b
kyle-escobar <kslrtips@gmail.com>
2020-09-21 02:31:32 -0400

    Add long const execution

--- asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/LCONST_0.kt
+++ asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/LCONST_0.kt
@@ -5,9 +5,15 @@ import org.objectweb.asm.MethodVisitor
 import org.spectral.asm.core.code.Instruction
 import org.spectral.asm.core.code.type.ConstantInstruction
 import org.spectral.asm.core.common.Opcode
+import org.spectral.asm.core.execution.ExecutionFrame
+import org.spectral.asm.core.execution.value.LongValue
 
 @Opcode(value=9)
 class LCONST_0 : Instruction(9) {
+
+  override fun execute(frame: ExecutionFrame) {
+    frame.push(LongValue(0L))
+  }
 
   override fun accept(visitor: MethodVisitor) {
     visitor.visitInsn(opcode)
--- asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/LCONST_1.kt
+++ asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/LCONST_1.kt
@@ -5,9 +5,15 @@ import org.objectweb.asm.MethodVisitor
 import org.spectral.asm.core.code.Instruction
 import org.spectral.asm.core.code.type.ConstantInstruction
 import org.spectral.asm.core.common.Opcode
+import org.spectral.asm.core.execution.ExecutionFrame
+import org.spectral.asm.core.execution.value.LongValue
 
 @Opcode(value=10)
 class LCONST_1 : Instruction(10) {
+
+  override fun execute(frame: ExecutionFrame) {
+    frame.push(LongValue(1L))
+  }
 
   override fun accept(visitor: MethodVisitor) {
     visitor.visitInsn(opcode)
