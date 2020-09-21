54dfb3f7ceb0a999fde22a7b71355eca138df87c
kyle-escobar <kslrtips@gmail.com>
2020-09-21 17:08:34 -0400

    Fix two forgoten opcodes

--- asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/NOP.kt
+++ asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/NOP.kt
@@ -4,16 +4,9 @@ import kotlin.String
 import org.objectweb.asm.MethodVisitor
 import org.spectral.asm.core.code.Instruction
 import org.spectral.asm.core.common.Opcode
-import org.spectral.asm.core.execution.ExecutionFrame
 
 @Opcode(value=0)
 class NOP : Instruction(0) {
-
-  override fun execute(frame: ExecutionFrame) {
-    /*
-     * DO NOTHING.
-     */
-  }
 
   override fun accept(visitor: MethodVisitor) {
     visitor.visitInsn(opcode)
