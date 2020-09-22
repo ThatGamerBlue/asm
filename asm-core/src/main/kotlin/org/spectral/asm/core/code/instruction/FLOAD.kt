c70e9a344bf2185299a03271f488e1e905121d3b
Kyle Escobar <kslrtips@gmail.com>
2020-09-22 08:48:50 -0400

    Add primitive loading execution

--- asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/FLOAD.kt
+++ asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/FLOAD.kt
@@ -6,11 +6,16 @@ import org.spectral.asm.core.code.Instruction
 import org.spectral.asm.core.code.type.VarInstruction
 import org.spectral.asm.core.code.type.InstructionType
 import org.spectral.asm.core.common.Opcode
+import org.spectral.asm.core.execution.Frame
 
 @Opcode(value=23)
 class FLOAD(override val index: Int) : Instruction(23), VarInstruction {
 
   override val type = InstructionType.FLOAT
+
+  override fun execute(frame: Frame) {
+    frame.push(frame.load(index).copy())
+  }
 
   override fun accept(visitor: MethodVisitor) {
     visitor.visitVarInsn(opcode, index)
