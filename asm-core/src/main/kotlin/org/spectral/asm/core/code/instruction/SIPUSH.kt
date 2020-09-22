dd6c9729ca4e11dbcebc5f097297b7e3dcdd86d0
Kyle Escobar <kslrtips@gmail.com>
2020-09-22 08:28:55 -0400

    Add sipush instruction execution

--- asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/SIPUSH.kt
+++ asm-core/src/main/kotlin/org/spectral/asm/core/code/instruction/SIPUSH.kt
@@ -5,9 +5,15 @@ import org.objectweb.asm.MethodVisitor
 import org.spectral.asm.core.code.Instruction
 import org.spectral.asm.core.code.type.IntInstruction
 import org.spectral.asm.core.common.Opcode
+import org.spectral.asm.core.execution.Frame
+import org.spectral.asm.core.execution.value.ShortValue
 
 @Opcode(value=17)
 class SIPUSH(override val operand: Int) : Instruction(17), IntInstruction {
+
+  override fun execute(frame: Frame) {
+    frame.push(ShortValue(operand.toShort()))
+  }
 
   override fun accept(visitor: MethodVisitor) {
     visitor.visitIntInsn(opcode, operand)
